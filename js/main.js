9065d1b87aad565c222f056e1b0cbd302f07b4a3
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-01-25 14:18:34 +0100

    Add js for console.log

--- js/main.js
+++ js/main.js
@@ -3,3 +3,28 @@
 // ma che per i multipli di 3 stampi in console "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
 // Per i numeri che sono sia multipli di 3 che di 5 stampi in console "FizzBuzz".
 
+for (let i = 0; i <= 100; i++) {
+
+    if (i % 5 == 0 && i % 3 == 0) {
+
+        console.log('FizzBuzz');
+
+    }
+    else if (i % 3 == 0) {
+
+        console.log('Fizz');
+
+    }
+    else if (i % 5 == 0) {
+
+        console.log('Buzz');
+
+    }
+    else {
+
+        console.log(i);
+
+    }
+
+
+}
